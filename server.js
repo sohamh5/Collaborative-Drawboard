d94b133b5a9053049fbca0004537a70f2d2e8c8b
sohamh5 <sohamhalarnkar22@gmail.com>
2025-04-26 23:53:45 +0530

    Fix: added root route to handle 502 error

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const path = require('path');
 
-app.use(express.static(path.join(__dirname)));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 io.on('connection', (socket) => {
   console.log('A user connected');
